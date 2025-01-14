be3e71e138be4982ac15c657c64a181682bd24e7
Dmytro Varchuk <loltop792@gmail.com>
2025-01-14 15:01:24 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,72 +3,181 @@
 
 using namespace std;
 
-void split(const int* array, int size, int* positive_array, int* negative_array, int* zero_array, int* pos_count, int* neg_count, int* zero_count) {
-    *pos_count = 0;
-    *neg_count = 0;
-    *zero_count = 0;
-
-    for (int i = 0; i < size; i++) {
-        if (array[i] > 0) {
-            positive_array[(*pos_count)++] = array[i];
-        }
-        else if (array[i] < 0) {
-            negative_array[(*neg_count)++] = array[i];
-        }
-        else {
-            zero_array[(*zero_count)++] = array[i];
-        }
-    }
-}
+void menu(int** data, int* length);
+void allocateMemory(int** data, int* length);
+void initializeArray(int* data, int length);
+void printArray(int* data, int length);
+void freeMemory(int*& data);
+void appendElement(int** data, int* length, int value);
+void insertElement(int** data, int* length, int value, int position);
+void removeElement(int** data, int* length, int position);
 
 int main() {
 
-    srand(time(0));
+	srand(time(0)); 
 
-
-    const int size = 5;
-    int array[size] = {};
-    for (int i = 0; i < size; i++) {
-        array[i] = rand() % 7 - 3;
-        cout << array[i] << " ";
+    int* data = nullptr;
+    int length = 0;
+    menu(&data, &length);
+    if (data != nullptr) {
+        freeMemory(data);
     }
-
-    cout << endl;
-
-    int* positive_array = new int[size] {};
-    int* negative_array = new int[size] {};
-    int* zero_array = new int[size] {};
-
-    int* pos_count = new int;
-    int* neg_count = new int;
-    int* zero_count = new int;
-
-    split(array, size, positive_array, negative_array, zero_array, pos_count, neg_count, zero_count);
-
-    cout << "Positive: ";
-    for (int i = 0; i < *pos_count; i++) {
-        cout << positive_array[i] << " ";
-    }
-    cout << endl;
-
-    cout << "Negative: ";
-    for (int i = 0; i < *neg_count; i++) {
-        cout << negative_array[i] << " ";
-    }
-    cout << endl;
-
-    cout << "Zero: ";
-    for (int i = 0; i < *zero_count; i++) {
-        cout << zero_array[i] << " ";
-    }
-    cout << endl;
-
-    delete[] positive_array;
-    delete[] negative_array;
-    delete[] zero_array;
-    delete pos_count;
-    delete neg_count;
-    delete zero_count;
-
     return 0;
+}
+
+void menu(int** data, int* length) {
+
+    int choice;
+    do {
+        cout << "Menu:\n";
+        cout << "1. Allocate memory\n";
+        cout << "2. Initialize array\n";
+        cout << "3. Print array\n";
+        cout << "4. Append element\n";
+        cout << "5. Insert element at position\n";
+        cout << "6. Remove element from position\n";
+        cout << "7. Free memory and exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        system("cls");
+
+        switch (choice) {
+        case 1:
+            allocateMemory(data, length);
+            break;
+        case 2:
+            if (*data == nullptr) {
+                cout << "Array not allocated. Please allocate memory first.\n";
+            }
+            else {
+                initializeArray(*data, *length);
+            }
+            break;
+        case 3:
+            if (*data == nullptr) {
+                cout << "Array not allocated. Please allocate memory first.\n";
+            }
+            else {
+                printArray(*data, *length);
+            }
+            break;
+        case 4: {
+            if (*data == nullptr) {
+                cout << "Array not allocated. Please allocate memory first.\n";
+            }
+            else {
+                int value;
+                cout << "Enter value to append: ";
+                cin >> value;
+                appendElement(data, length, value);
+            }
+            break;
+        }
+        case 5: {
+            if (*data == nullptr) {
+                cout << "Array not allocated. Please allocate memory first.\n";
+            }
+            else {
+                int value, position;
+                cout << "Enter value to insert: ";
+                cin >> value;
+                cout << "Enter position: ";
+                cin >> position;
+                insertElement(data, length, value, position);
+            }
+            break;
+        }
+        case 6: {
+            if (*data == nullptr) {
+                cout << "Array not allocated. Please allocate memory first.\n";
+            }
+            else {
+                int position;
+                cout << "Enter position to remove: ";
+                cin >> position;
+                removeElement(data, length, position);
+            }
+            break;
+        }
+        case 7:
+            freeMemory(*data);
+            cout << "Exiting...\n";
+            break;
+        default:
+            cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 7);
+}
+
+void allocateMemory(int** data, int* length) {
+    if (*data != nullptr) {
+        delete[] * data;
+    }
+    cout << "Enter the size of the array: ";
+    cin >> *length;
+    *data = new int[*length];
+}
+
+void initializeArray(int* data, int length) {
+    for (int i = 0; i < length; i++) {
+        data[i] = rand() % 100 - 1;;
+    }
+}
+
+void printArray(int* data, int length) {
+    for (int i = 0; i < length; i++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
+}
+
+void freeMemory(int*& data) {
+    delete[] data;
+    data = nullptr;
+}
+
+void appendElement(int** data, int* length, int value) {
+    int* newData = new int[*length + 1];
+    for (int i = 0; i < *length; i++) {
+        newData[i] = (*data)[i];
+    }
+    newData[*length] = value;
+    delete[] * data;
+    *data = newData;
+    *length += 1;
+}
+
+void insertElement(int** data, int* length, int value, int position) {
+    if (position < 0 || position > *length) {
+        cout << "Position out of range.\n";
+        return;
+    }
+    int* newData = new int[*length + 1];
+    for (int i = 0; i < position; i++) {
+        newData[i] = (*data)[i];
+    }
+    newData[position] = value;
+    for (int i = position + 1; i < *length + 1; i++) {
+        newData[i] = (*data)[i - 1];
+    }
+    delete[] * data;
+    *data = newData;
+    *length += 1;
+}
+
+void removeElement(int** data, int* length, int position) {
+    if (position < 0 || position >= *length) {
+        cout << "Position out of range.\n";
+        return;
+    }
+    int* newData = new int[*length - 1];
+    for (int i = 0; i < position; i++) {
+        newData[i] = (*data)[i];
+    }
+    for (int i = position; i < *length - 1; i++) {
+        newData[i] = (*data)[i + 1];
+    }
+    delete[] * data;
+    *data = newData;
+    *length -= 1;
 }
